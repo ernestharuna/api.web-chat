99aa13e4172da3d62a336bf80580c49e9a3f8027
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2025-07-25 15:42:43 +0100

    Add relationships for Inbox and Message models; update User model for inboxes relationship

--- app/Models/Inbox.php
+++ app/Models/Inbox.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Inbox extends Model
 {
     use HasUlids;
+    protected $fillable = [];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function message()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasUlids;
+
+    protected $fillable = [
+        'inbox_id',
+        'user_id',
+        'content',
+        'is_read',
+    ];
+
+    public function inbox()
+    {
+        return $this->belongsTo(Inbox::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
